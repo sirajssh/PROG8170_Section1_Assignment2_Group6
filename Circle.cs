51cfbe7681c4a4c744324ea3c7e6bf055e4a1b4b
Syed Sirajul Hasan <syedsirajulhasan@conestogac.on.ca>
2023-02-04 15:59:00 -0500

    Completed Circle class

---
+++ Circle.cs
@@ -0,0 +1,60 @@
+﻿/*
+Assignment-2 SQAT
+PROG-8170 SEC-1
+
+Done by: Group-6
+Syed Sirajul Hasan - 8836760
+Piyush Mansukhbhai Monpara - 8784742
+
+File: Circle.cs
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROG8170_Section1_Assignment2_Group6
+{
+        public class Circle
+        {
+            private int radius;
+
+            public Circle()
+            {
+                radius = 1;
+            }
+
+            public Circle(int radius)
+            {
+                this.radius = radius;
+            }
+
+            public int GetRadius()
+            {
+                return radius;
+            }
+
+            public void SetRadius(int radius)
+            {
+                if (radius >= 1)
+                {
+                    this.radius = radius;
+                }
+            }
+
+            public double GetCircumference()
+            {
+                double circumference = 2 * Math.PI * this.radius;
+                return Math.Round(circumference, 2);
+            }
+
+            public double GetArea()
+            {
+                double area = Math.PI * this.radius * this.radius;
+                return Math.Round(area, 2);
+            }
+        }
+
+}
